3b4242a7cb6bc2b3f4a0a805eaecbc639472a7a2
walid <walid.boudchiche@gmail.com>
2024-08-06 13:28:12 +0100

    first commit

---
+++ inde.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+   header("Location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>User Dashboard</title>
+</head>
+<body>
+    <div class="banner"></div>
+    <div class="container">
+    <img src="Logo-ims.png" alt="">
+        <h1>Welcome to Ibn Tofail University</h1>
+        <a href="logout.php" class="btn btn-primary">Login</a>
+    </div>
+</body>
+</html>
